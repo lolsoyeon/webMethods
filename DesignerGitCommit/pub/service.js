c78418d30f55229c5c3bea5daa6807af2d9f0bd0
lolsoyeon <tivm8012@naver.com>
2026-02-05 14:57:14 +0900

    acc..commit

---
+++ DesignerGitCommit/pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://DESKTOP-8KQ5B5R:5557/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "05-02-2026 14:44:10 YAKT";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"DesignerGitCommit","createdDate":"05-02-2026 12:49:56 YAKT","version":"1.0"};
+}
+});
